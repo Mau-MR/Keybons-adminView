3f7e862ade45b5b4a481c3c099a333d412dfa10f
Mauricio <morrismerida@hotmail.com>
2021-01-06 11:40:25 -0600

    Finished some components and start of the web client to connect to grpc server

--- src/routes/landingPage.jsx
+++ src/routes/landingPage.jsx
@@ -1,22 +1,28 @@
-import "./landigPage.css"
+import "./landigPage.css";
 import SideBtn from "../containers/SideBtnContainer";
-import ProgressWidget from "../containers/ProgressWidgetContainer";
+import ProgressWidget from "../components/ProgressWidget";
+import { AccountCircle } from "@material-ui/icons";
 
 //This is going to be the main root of the adminView
 const LandingPage = () => {
-    return (
-        <div className="main-page">
-            <div className="navbar">navbar</div>
-            <div className="main-content">
-                <div className="left-bar side-bars">
-                    <SideBtn/>
-                </div>
-                <div className="content-bar">
-                    <ProgressWidget/>
-                </div>
-                <div className="right-bar side-bars">rightBar</div>
-            </div>
+  return (
+    <div className="main-page">
+      <div className="navbar">navbar</div>
+      <div className="main-content">
+        <div className="left-bar side-bars">
+          <SideBtn />
         </div>
-    );
-}
+        <div className="content-bar">
+          <ProgressWidget
+            icon={<AccountCircle />}
+            percentage={100}
+            activity={"make the inventory"}
+          />
+        </div>
+        <div className="right-bar side-bars">rightBar</div>
+      </div>
+    </div>
+  );
+};
 export default LandingPage;
+
---
+++ src/routes/views/Dashboard.js
@@ -0,0 +1,6 @@
+import "./Dashboard.css";
+
+const Dashboard = (props) => {
+  return <div className="Dashboard-container"></div>;
+};
+export default Dashboard;
